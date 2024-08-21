f9298815dc5a37f653c11076dbf9ece3b7d40e69
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2024-08-21 19:52:35 +0530

    Create privateaccessspecifier.cpp

---
+++ privateaccessspecifier.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class student
+{
+  private:
+char name[20];
+public:
+            char branch[10];
+                        int sem,roll_no;
+                void input(){
+                cout<<"enter your name";
+                cin>>name;
+              cout<<"enter your branch";
+                cin>>branch;
+                     cout<<"enter your  roll_no";
+                cin>>roll_no;
+}
+
+};
+class teacher:public student{
+public:
+    void display(){
+    cout<<name<<endl<<branch<<endl<<roll_no;}
+};
+int main()
+{
+        student s1;
+       teacher t1;
+        t1.input();
+        t1.display();
+}
