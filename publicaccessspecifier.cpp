3aa1b8e413393a9dede0925aa7b498554b7da6b2
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2024-08-21 19:55:09 +0530

    Create publicaccessspecifier.cpp

---
+++ publicaccessspecifier.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class student
+{
+  public:
+char name[20];
+
+            char branch[10];
+                        int roll_no;
+                void input(){
+                cout<<"enter your name";
+                cin>>name;
+              cout<<"enter your branch";
+                cin>>branch;
+                     cout<<"enter your roll_no";
+                cin>>roll_no;
+}
+};
+class teacher:public student{
+public:
+    void display(){
+    cout<<name<<endl<<branch<<endl<<roll_no;}
+};
+int main()
+{
+        student s1;
+       teacher t1;
+        t1.input();
+        t1.display();
+}
